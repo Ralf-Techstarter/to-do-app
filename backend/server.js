a5a5b5a2da69ecb00843268b0a8d61fe92d562e8
Ralf-Techstarter <ralf.juliusson@tn.techstarter.de>
2025-03-05 12:54:25 +0100

    Datenbank erstellt, post request zu item hinzufügen

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,24 @@
 const express = require('express');
 
+const sqlite3 = require('sqlite3');
+
+const bodyParser = require('body-parser')
+
 const app = express();
+
+const db = new sqlite3.Database('./tasks.db');
+
+app.use(bodyParser.json());
+
+db.run('CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, completed BOOLEAN DEFAULT 0)')
+
+
+app.post('/add', (req, res) => {
+    db.run('INSERT INTO tasks (title) VALUES (?)', [req.body.title], function () {
+        res.json({tag: "Mittwoch", bald_wirds: "Mittagspause"});
+    });
+});
+
 
 app.get('/', (req, res) => {
     res.send('request received')
@@ -10,6 +28,7 @@ app.get('/', (req, res) => {
 app.listen(3500, 'localhost', () => {
 console.log("bald ist Mittagspause")
 })
+
 
 
 
